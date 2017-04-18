2110d1561a49314afc173ab041319f5bb01ffdff
wylanneely@gmail.com <aliamneely@ALIAs-MacBook-Pro.local>
2017-04-18 10:52:33 -0600

    add pokemon model and JSON Failable initializer

--- Pokedex/Pokedex/PokedexViewController.swift
+++ Pokedex/Pokedex/PokedexViewController.swift
@@ -8,28 +8,22 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
+class PokedexViewController: UIViewController, UISearchBarDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var nameLabel: UILabel!
+  
+    @IBOutlet weak var idLabel: UILabel!
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        <#code#>
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
 }
---
+++ Pokedex/Pokedex/Pokemon.swift
@@ -0,0 +1,49 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by ALIA M NEELY on 4/18/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Pokemon {
+    
+    let name: String
+    let id: Int
+    let abilities: [String]
+  
+}
+
+
+//MARK: - JSON
+extension Pokemon {
+    
+    private static let kName = "name"
+    private static let kId = "id"
+    private static let kAbilities = "abilities"
+    
+    init?(pokemonDictionary: [String: AnyObject]) {
+        guard let name = pokemonDictionary[Pokemon.kName] as? String,
+        let id = pokemonDictionary[Pokemon.kId] as? Int,
+            let abilitityDiction = pokemonDictionary[Pokemon.kAbilities] as? [[String: AnyObject]] else {return nil}
+        
+        
+        // the optional chaining "?" searches for "name" key inside of the "ability" keys dictionary
+        let abilities = abilitityDiction.flatMap{ $0["ability"]?["name"] as? String }
+        
+        self.init(name: name, id: id, abilities: abilities)
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+
